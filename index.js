673f032ba8c49356811421b1098d047ebf204f74
Joshua Allford <joshallfordnanook@gmail.com>
2021-08-21 02:31:47 +0100

    Assignment Complete!

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+const cards = ["Lisa", "Kaitlin", "Jan",];
+
+function writeCards() {
+
+    const newCards = [];
+
+    for (let i = 0; i < cards.length; i++) {
+        newCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+
+    }
+
+    return newCards;
+
+
+}
+
+writeCards(cards);
+
+function countDown() {
+
+    let countDown = 10;
+
+    while (countDown > -1) {
+        console.log(countDown--);
+    }
+
+
+}
+
+countDown()
